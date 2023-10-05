2fa04348e3ee87287574f9f4b6d05d193b25ab66
Anna Tischenko <tischenko.a.13@gmail.com>
2023-10-05 17:53:02 +0300

    fix database tables creation

--- handlers/custom_handlers/hello.py
+++ handlers/custom_handlers/hello.py
@@ -1,11 +1,13 @@
 from loader import bot
-from models.user import User
+from models.base_model import User
 
 
 @bot.message_handler(commands=['hello-world'])
 def handle_hello_world(message):
+
     user_id = message.from_user.id
     request_text = message.text
+
     response_text = 'Пока вас не было, курс валют в мире изменился.\n' \
                     'Чтобы узнать список доступных команд введите /help'
 
@@ -17,8 +19,10 @@ def handle_hello_world(message):
 
 @bot.message_handler(func=lambda message: message.text.lower() == 'привет' or message.text.lower() == 'здравствуйте')
 def handle_privet(message):
+
     user_id = message.from_user.id
     request_text = message.text
+
     response_text = 'Приветствую вас 👋\n' \
                     'Чтобы узнать список доступных команд введите /help'
 
