aae69412d38881e505307f649ff0b6aca0a2332d
Anna Tischenko <tischenko.a.13@gmail.com>
2023-10-05 16:33:02 +0300

    Revert "init"
    This reverts commit 7b5f7d9d52f31f2c9cf3a9a77afa46eb9ab7297c.

--- loader.py
+++
@@ -1,9 +0,0 @@
-from telebot import TeleBot
-from telebot.storage import StateMemoryStorage
-from config_data import config
-
-storage = StateMemoryStorage()
-bot = TeleBot(token=config.BOT_TOKEN, state_storage=storage)
-
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
--- models/base_model.py
+++
@@ -1,8 +0,0 @@
-import peewee as pw
-
-db = pw.SqliteDatabase('test_3.pw')
-
-
-class BaseModel(pw.Model):
-    class Meta:
-        database = db
